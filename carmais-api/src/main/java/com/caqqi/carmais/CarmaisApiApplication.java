4feb25fb6c1b66821af497655327ebe8a61348df
bruno-luz <bruno.luz@preventsenior.com.br>
2020-07-07 23:28:08 -0300

    Criando projetos api e front

---
+++ carmais-api/src/main/java/com/caqqi/carmais/CarmaisApiApplication.java
@@ -0,0 +1,13 @@
+package com.caqqi.carmais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmaisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmaisApiApplication.class, args);
+	}
+
+}
---
+++ carmais-api/src/test/java/com/caqqi/carmais/CarmaisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caqqi.carmais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmaisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
